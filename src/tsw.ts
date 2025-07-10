76ca88209da9585512e570ea02bed7cdec70f430
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-10 10:34:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/tsw.ts
+++ src/tsw.ts
@@ -1,5 +1,19 @@
 // let add = 3 + [];
 // console.log(add);
 
-let obj = {};
-console.log(obj);
+// let obj = {};
+// console.log(obj);
+
+console.log('Hello TypeScript!');
+
+// types:
+// - boolean = Boolean
+// - number = Number
+// - string = String
+
+function squareOf(n: number) {
+    return n * n;
+}
+console.log(squareOf(2));
+console.log(squareOf(1.1));
+// console.log(squareOf('z'));
